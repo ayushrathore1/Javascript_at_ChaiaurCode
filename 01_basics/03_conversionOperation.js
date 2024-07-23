9053efc05753ed1d0e1d375cf09197ba321d689d
ayushrathore1 <131510947+ayushrathore1@users.noreply.github.com>
2024-07-23 09:59:18 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,10 +12,18 @@ let valueInNumber = Number(score)
 // "33abc" => NaN(Not a Number)
 // true => 1; false => 0
 
-//let isLoggedIn = 1
-let isLoggedIn = ""
+let isLoggedIn = "Hitesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn);
+// 1 => true; 0 => false
+// "" => false; "Hitesh" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
 

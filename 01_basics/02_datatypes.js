ba582b950d59a3950598356c40d7c000c1114911
ayushrathore1 <131510947+ayushrathore1@users.noreply.github.com>
2024-07-23 09:19:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+// Caution : If got confused about comments then once remove the comment and try to run it and then understand.
+
+"use strict"; // treat all JS code as newer version of Javascript.
+
+// alert(3+3); We are using Node.js not Browser.
+
+console.log(3+3)
+
+// Code Readability should be on high priority
+
+console.log("Jai Bhole Baba")
+
+
+let name = "Hitesh"
+let age = 18
+let isLoggedin = false
+let state;
+
+// number =>  2 to the power 53
+// bigint 
+//string => ""
+// boolean => True/False
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+

e0777eb7897d1d17a9fbbc9c8d378b693c3278df
ayushrathore1 <131510947+ayushrathore1@users.noreply.github.com>
2024-08-01 12:11:19 +0000

    some change

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,14 @@
-let score = 33
+let score = "3fdsd3"
 
-console.log(typeof score);
-console.log(typeof (score));
+console.log(typeof score); // number
+console.log(typeof (score)); // number
+
+
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(valueInNumber); 
+
+
+
+// Nan => Not a Number

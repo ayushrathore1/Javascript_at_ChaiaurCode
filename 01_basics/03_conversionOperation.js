3dd0b5c9582a9cfd4dd19ac9874f815e39bfe332
ayushrathore1 <131510947+ayushrathore1@users.noreply.github.com>
2024-08-01 11:15:43 +0000

    step 1 towards 03

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,29 +1,4 @@
-let score = "Hitesh"
+let score = 33
 
-// console.log(typeof score);
-// console.log(typeof (score));
-
-let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber)
-
-
-// "33" => 33
-// "33abc" => NaN(Not a Number)
-// true => 1; false => 0
-
-let isLoggedIn = "Hitesh"
-
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-
-// console.log(booleanIsLoggedIn);
-// 1 => true; 0 => false
-// "" => false; "Hitesh" => true
-
-
-let someNumber = 33
-
-let stringNumber = String(someNumber)
-
-console.log(typeof stringNumber);
-
+console.log(typeof score);
+console.log(typeof (score));

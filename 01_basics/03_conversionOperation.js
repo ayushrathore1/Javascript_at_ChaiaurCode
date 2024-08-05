875c421032d0a4f4b823572ff5954fdf0ca1fdd4
ayushrathore1 <131510947+ayushrathore1@users.noreply.github.com>
2024-08-05 04:26:14 +0000

    learning Conversions through Investigation Study

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,30 @@
-let score = "3fdsd3"
+let score = "hitesh"
 
-console.log(typeof score); // number
-console.log(typeof (score)); // number
+//console.log(typeof score); // number
+//console.log(typeof (score)); // number
 
-
+// Conversion into Number
 
 let valueInNumber = Number(score)
-console.log(valueInNumber);
-console.log(valueInNumber); 
-
-
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); 
 
 // Nan => Not a Number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "hitesh"
+
+// Conversion into Boolean
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+ // 1 => true
+ // 0 => false
+ // "" => false
+ // "something" => true

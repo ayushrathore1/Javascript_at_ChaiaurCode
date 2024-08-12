91184077bab9dbee17070736304aee51819c4556
ayushrathore1 <131510947+ayushrathore1@users.noreply.github.com>
2024-08-12 09:02:08 +0000

    string1

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name  = "Ayush"
+const repoCount = 50
+
+// console.log(name + repoCount + "value");
+
+console.log(`Hello My name is ${name} and my repo count is ${repoCount}.`);
+
+const gameName = new String("hiteshhc")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);

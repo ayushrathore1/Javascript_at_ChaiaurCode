74471f818bce51cd9b440002bb41d9110943667f
ayushrathore1 <131510947+ayushrathore1@users.noreply.github.com>
2024-10-09 07:36:06 +0000

    Restart 2

---
+++ Restart/01_basics/03_conversionOperation.js
@@ -0,0 +1,222 @@
+/*
+let score = 33
+
+console.log(typeof (score)); // number
+console.log(typeof score); // number
+*/
+
+/*
+
+let score = "33"
+
+console.log(typeof (score)); // string
+console.log(typeof score); // string
+
+let valueInNumber = Number(score) // Conversion in number
+console.log(typeof valueInNumber); // number 
+*/
+
+/* 
+
+let score = "33abc"
+
+console.log(typeof (score)); // string
+console.log(typeof score); // string
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); // NaN => Not a Number 
+
+*/
+
+
+
+/* 
+
+let score = null
+
+console.log(typeof (score)); // object
+console.log(typeof score); // object
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); // 0 
+
+*/
+
+/*
+
+let score = undefined
+
+console.log(typeof (score)); // undefined
+console.log(typeof score); // undefined
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); // NaN 
+
+*/
+
+
+/*
+
+let score = true
+
+console.log(typeof (score)); // boolean
+console.log(typeof score); // boolean
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); // 1
+
+*/
+
+
+/* 
+
+let score = false
+
+console.log(typeof (score)); // boolean
+console.log(typeof score); // boolean
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); // 0 
+
+*/
+
+
+/*
+
+let score = "Jai Ho"
+
+console.log(typeof (score)); // string
+console.log(typeof score); // string
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); // NaN
+
+*/
+
+
+//          Summary            //
+
+/*
+
+"33" => 33
+"33abc" => NaN
+"true" => 1
+"false" => 0
+undefined => NaN
+string => NaN
+null => 0
+
+*/
+
+
+
+/*
+
+Additionaly;
+    typeof NaN => 
+
+console.log(typeof NaN); // number
+
+If We Print NaN =>
+
+console.log(NaN); // NaN
+
+*/
+
+
+/* 
+
+let isLoggedIn = 1
+
+console.log(typeof isLoggedIn); // number
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn); // true 
+
+*/
+
+
+
+/* let isLoggedIn = 0
+
+console.log(typeof isLoggedIn); // number
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn); // false
+
+*/
+
+
+/*
+
+let isLoggedIn = ""
+
+console.log(typeof isLoggedIn); // string
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn); // false
+
+*/
+
+/*
+
+let isLoggedIn = "Jai Ho"
+
+console.log(typeof isLoggedIn); // string
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn); // true
+
+*/
+
+/*
+
+let isLoggedIn = 416564 // any number positive or negative
+
+console.log(typeof isLoggedIn); // number
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn); // true
+
+*/
+
+/* 
+let isLoggedIn = null
+
+console.log(typeof isLoggedIn); // object
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn); // false
+
+*/
+
+/* 
+
+let isLoggedIn = undefined
+
+console.log(typeof isLoggedIn); // undefined
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn); // false 
+
+*/
+
+let someNumber = 153
+
+console.log(typeof someNumber); // number
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber); // String
+
+console.log(someNumber); // 153
+
+
+
+
+//      To be continued      //

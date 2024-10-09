b59cb470fc062ce6c6973a0ccf3d3b212969ce7f
ayushrathore1 <131510947+ayushrathore1@users.noreply.github.com>
2024-10-09 06:34:39 +0000

    Restart 1

---
+++ Restart/01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 15264
+let accountEmail = "Ayush@google.com"
+var accountPassword = "12535"
+accountCity = "jaipur"
+let accountState;
+
+
+// accountId = 2  // Not allowed to change const
+// console.log(aacountId);
+
+
+accountEmail = "hdb@google.com"
+accountPassword = "6462"
+accountCity = "Kota"
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
+/*
+     prefer Not TO Use var 
+     because of issue in block scope and functional scope
+*/
---
+++ Restart/01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert("hello") // We are using node js, not browser
+
+console.log(3 + 3); 
+ // Code readability should be high
+console.log("Jai Ho");
+
+
+let name = "Ayush"
+let age = 18
+let isLoggedIn = false
+let temp = null
+
+//  range of datatype number => 2 to the power 53
+
+
+/*              Primitive Datatypes                  */
+
+// bigint => have greater range than number
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Ayush"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
+
